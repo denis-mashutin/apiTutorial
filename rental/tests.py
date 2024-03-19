ccf8bd65ff1d3f1977c04b32fac530d17d271c80
Denis Mashutin <denis.mashutin@jetbrains.com>
2024-03-19 12:50:54 +0100

    Added auto-generated tests

--- rental/tests.py
+++ rental/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from rental.models import Offer
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='password')
+
+    def test_offer_list(self):
+        # Login
+        self.client.login(username='testuser', password='password')
+        url = reverse('offer-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_offer_details(self):
+        # Login and create a sample offer
+        self.client.login(username='testuser', password='password')
+        offer = Offer.objects.create(name='Test Offer', author=self.user)
+        url = reverse('offer-detail', args=[offer.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_list(self):
+        url = reverse('user-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_detail(self):
+        url = reverse('user-detail', args=[self.user.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
