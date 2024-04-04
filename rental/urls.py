e0a56e88bde8ed370d089fedaf89d25dc3170732
Denis Mashutin <denis.mashutin@jetbrains.com>
2024-04-04 11:25:57 +0200

    security update

--- rental/urls.py
+++ rental/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from rental import views
 
 urlpatterns = [
-    path('offers/', views.OfferList.as_view()),
-    path('offers/<int:pk>/', views.OfferDetails.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetails.as_view()),
+    path('offers/', views.OfferList.as_view(), name='offer-list'),
+    path('offers/<int:pk>/', views.OfferDetails.as_view(), name='offer-details'),
+    path('users/', views.UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', views.UserDetails.as_view(), name='user-details'),
 ]
